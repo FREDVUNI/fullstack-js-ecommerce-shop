0227f936c5d4e1e495337b6b9e0f57fbaa7b7b0c
TAATA EARL <fredvuni809@gmail.com>
2023-01-23 17:02:28 +0300

    setup for hosting on render

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
     res.status(200) 
